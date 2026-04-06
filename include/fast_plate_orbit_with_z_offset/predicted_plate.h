01c385fca875ecfc18d83ab9a5f71b340f827399
aruw-jetson-squirtle <robomstr@uw.edu>
2026-04-05 20:49:01 -0700

    Remove velocity from predicted plate as unused

--- include/fast_plate_orbit_with_z_offset/predicted_plate.h
+++ include/fast_plate_orbit_with_z_offset/predicted_plate.h
@@ -9,16 +9,14 @@ namespace fast_plate_orbit_with_z_offset {
 class predicted_plate {
  private:
   Eigen::Vector3f position_;
-  Eigen::Vector3f velocity_;
 
  public:
   PF_TARGET_ATTRS [[nodiscard]] const Eigen::Vector3f& position() const noexcept { return position_; }
-  PF_TARGET_ATTRS [[nodiscard]] const Eigen::Vector3f& velocity() const noexcept { return velocity_; }
 
-  PF_TARGET_ATTRS predicted_plate() noexcept : position_{Eigen::Vector3f::Zero()}, velocity_{Eigen::Vector3f::Zero()} {}
+  PF_TARGET_ATTRS predicted_plate() noexcept : position_{Eigen::Vector3f::Zero()} {}
 
-  PF_TARGET_ATTRS predicted_plate(const Eigen::Vector3f& position, const Eigen::Vector3f& velocity) noexcept
-      : position_{position}, velocity_{velocity} {}
+  PF_TARGET_ATTRS predicted_plate(const Eigen::Vector3f& position) noexcept
+      : position_{position} {}
 };
 
 }  // namespace fast_plate_orbit_with_z_offset
--- include/fast_plate_orbit_with_z_offset/prediction.h
+++ include/fast_plate_orbit_with_z_offset/prediction.h
@@ -110,11 +110,7 @@ class prediction {
           helper::rpad_zero(center_) +
           (Eigen::Vector3f{} << value.radius * cosf(angle), value.radius * sinf(angle), value.z_coordinate).finished();
 
-      const Eigen::Vector3f predicted_plate_velocity =
-          helper::rpad_zero(center_velocity_) +
-          orientation_velocity_ * (Eigen::Vector3f{} << -value.radius * sinf(angle), value.radius * cosf(angle), 0.0f).finished();
-
-      return predicted_plate(predicted_plate_position, predicted_plate_velocity);
+      return predicted_plate(predicted_plate_position);
     });
   }
 
